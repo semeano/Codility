8580ccd41b4fb21f5cb4f7a5b5af02d0b7cafa60
Pedro Semeano <semeano@gmail.com>
2016-05-17 11:50:15 +0100

    StoneWall new solution.

--- lesson7.js
+++ lesson7.js
@@ -28,7 +28,8 @@ function Brackets(S) {
 	return stack.length === 0 ? 1 : 0;
 }
 
-// N voracious fish are moving along a river. Calculate how many fish are alive.
+// N voracious fish are moving along a river. Some upstream (0) others downstream (1). When they meet, the largest eats the smallets one. Calculate how many fish are left alive.
+// A=[4,3,2,1,5], B=[0,1,0,0,0] => 2
 function Fish(A, B) {
 	if (A.length === 1) return 1;
 	for (var i = 1; i < A.length; i++) {
@@ -46,6 +47,47 @@ function Fish(A, B) {
 		}
 	}
 	return A.length;
+}
+
+// Cover "Manhattan skyline" using the minimum number of blocks. Save the active blocks and remove when a lower height is found. At the end sum the remaining active blocks to the current result.
+// [8,8,5,7,9,8,7,4,8] => 7
+function StoneWall(H) {
+	var l = H.length;
+	if (l === 1) { return 1; }
+	var result = 0;
+	var stack = [];		// save active blocks
+	for (var i = 0; i < l; i++) {
+		if (H[i] < stack[stack.length-1]) {
+			// remove from stack lower active blocks
+			while (H[i] < stack[stack.length-1]) {
+				stack.pop();
+				result++;
+			}
+		}
+		if (H[i] > stack[stack.length-1] || stack.length === 0) {
+			stack.push(H[i]);
+		}
+	}
+	return result + stack.length;
+}
+
+// Determine whether given string of parentheses is properly nested.
+function Nesting(S) {
+	if (S === '') return 1;
+	var l = S.length;
+	if (l%2 !== 0 || S[0] !== '(' || S[l-1] !== ')') return 0;
+	var A = [];
+	for (var i = 0; i < S.length; i++) {
+		if (S[i] !== '(' && S[i] !== ')') return 0;
+		if (S[i] === '(') {
+			A.push(S[i]);
+		}
+		else {
+			if (A.length === 0) return 0;
+			A.splice(-1,1);
+		}
+	}
+	return A.length === 0 ? 1 : 0;
 }
 
 
